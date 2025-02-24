e4779f17ffb0762c4b79fff951b5f728e135d401
DZaytsev4 <deniszaytsev79524555@gmail.com>
2025-02-24 22:08:44 +0300

    relise

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,14 +4,52 @@
 #include <cstdint>
 #include "alg.h"
 
-
-TEST(st1, sumPrime1) {
-  uint64_t res = sumPrime(2000000);
-  uint64_t expected = 142913828922;
-  EXPECT_EQ(expected, res);
+TEST(checkPrimeTest, isPrime)
+{
+  EXPECT_TRUE(checkPrime(29));
 }
-TEST(st1, sumPrime2) {
-  uint64_t res = sumPrime(10);
-  uint64_t expected = 17;
-  EXPECT_EQ(expected, res);
+
+TEST(checkPrimeTest, isNotPrime)
+{
+  EXPECT_FALSE(checkPrime(25));
+}
+
+TEST(nPrimeTest, firstPrime)
+{
+  EXPECT_EQ(nPrime(1), 2);
+}
+
+TEST(nPrimeTest, fifthPrime)
+{
+  EXPECT_EQ(nPrime(5), 11);
+}
+
+TEST(nPrimeTest, tenthPrime)
+{
+  EXPECT_EQ(nPrime(10), 29);
+}
+
+TEST(nextPrimeTest, nextAfterFour)
+{
+  EXPECT_EQ(nextPrime(4), 5);
+}
+
+TEST(nextPrimeTest, nextAfterEleven)
+{
+  EXPECT_EQ(nextPrime(11), 13);
+}
+
+TEST(nextPrimeTest, nextAfterTwenty)
+{
+  EXPECT_EQ(nextPrime(20), 23);
+}
+
+TEST(sumPrimeTest, sumBelowTen)
+{
+  EXPECT_EQ(sumPrime(10), 17);
+}
+
+TEST(sumPrimeTest, sumBelowTwenty)
+{
+  EXPECT_EQ(sumPrime(20), 77);
 }
